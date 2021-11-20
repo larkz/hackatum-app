9b3a2238dc544cd526f9d401afb7f6c8a2705199
Eric Paul <eric.paul@tum.de>
2021-11-20 08:12:23 +0100

    Create initial project structure

---
+++ android/src/com/github/hackatum/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.github.hackatum;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.hackatum.Optimo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Optimo(), config);
+	}
+}
---
+++ desktop/src/com/github/hackatum/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.hackatum.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.hackatum.Optimo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Optimo(), config);
+	}
+}
