331f95cb4d135eb0c6a9ac9966419fb7a4271f68
larkz <liu.larkin@gmail.com>
2021-11-20 22:41:49 +0100

    integrated mcts4j

--- desktop/src/com/github/hackatum/desktop/DesktopLauncher.java
+++ desktop/src/com/github/hackatum/desktop/DesktopLauncher.java
@@ -5,10 +5,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.github.hackatum.Optimo;
 import com.github.hackatum.OptimoMDP;
 import java.util.Arrays;
-
 import java.util.List;
-
 import java.io.IOException;
+
+import ca.aqtech.mctreesearch4j.GenericSolver;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) throws IOException {
@@ -17,6 +17,10 @@ public class DesktopLauncher {
 		// OM.printIO();
 		OM.ingestGrocerySim();
 		OM.initialState();
+		GenericSolver solver = new GenericSolver(OM, 999, 0.28, 0.95, true);
+
+		solver.runTreeSearch(99);
+
 		// System.out.println(OM.actions(OM.initialState()));
 
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
