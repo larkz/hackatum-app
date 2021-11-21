23dd1ae54d3b3cfc9737b4f0bfd43f810e457218
Ricky Rong <ricky.rong@uwaterloo.ca>
2021-11-20 23:18:19 -0500

    Add tables to display optimal purchases, integration logic and fixes

--- core/src/com/github/hackatum/Optimo/OptimoGenerator.java
+++ core/src/com/github/hackatum/Optimo/OptimoGenerator.java
@@ -3,8 +3,7 @@ package com.github.hackatum.Optimo;
 import ca.aqtech.mctreesearch4j.GenericSolver;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class OptimoGenerator {
 
@@ -18,7 +17,7 @@ public class OptimoGenerator {
         priceMax = priceMaxInput;
     }
 
-    public void run() throws IOException {
+    public String run() throws IOException {
         List<String> articleClassListInput = Arrays.asList("Barley", "Apples");
         OptimoMDP OM = new OptimoMDP(articleClasses, weightMax, priceMax);
         // OM.printIO();
@@ -41,6 +40,24 @@ public class OptimoGenerator {
         System.out.println(stdoutStr[stdoutStr.length - 2]);
 
         String outputStr = stdoutStr[stdoutStr.length - 2];
+        return outputStr;
+    }
+
+    public List<Map<String, String>> parseOutput(String outputStr) {
+        outputStr = outputStr.substring(3);
+        List<Map<String, String>> parsedOutput = new ArrayList<>();
+        String[] elements = outputStr.split(" -> ");
+        for (int i = 0; i < elements.length - 1; i += 2) {
+            Map<String, String> row = new HashMap<>();
+            row.put("brand", elements[i]);
+            String[] values = elements[i+1].split(" ");
+            for (String param:values) {
+                String[] fvPair = param.split("\\|");
+                row.put(fvPair[0], String.format("%.2f", Float.parseFloat(fvPair[1])));
+            }
+            parsedOutput.add(row);
+        }
+        return parsedOutput;
     }
 
 
--- core/src/com/github/hackatum/Screens/ShoppingListScreen.java
+++ core/src/com/github/hackatum/Screens/ShoppingListScreen.java
@@ -6,12 +6,14 @@ import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.ui.List;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.github.hackatum.Optimo.Optimo;
+import com.github.hackatum.Optimo.OptimoGenerator;
 import com.github.hackatum.resources.GreenScore;
 import com.github.hackatum.resources.ShoppingList;
+
+import java.io.IOException;
+import java.util.Map;
 
 public class ShoppingListScreen extends ScreenAdapter {
 
@@ -19,7 +21,7 @@ public class ShoppingListScreen extends ScreenAdapter {
     //--Back arrow
     private static final float BACK_ARROW_WIDTH = 81;
     private static final float BACK_ARROW_HEIGHT = 57;
-    private static final float BACK_ARROW_X1 = 50;
+    private static final float BACK_ARROW_X1 = 40;
     private static final float BACK_ARROW_Y1 = 375;
     private static final float BACK_ARROW_X2 = BACK_ARROW_X1 + BACK_ARROW_WIDTH;
     private static final float BACK_ARROW_Y2 = BACK_ARROW_Y1 + BACK_ARROW_HEIGHT;
@@ -27,10 +29,38 @@ public class ShoppingListScreen extends ScreenAdapter {
     //--Add element to table
     private static final float PLUS_WIDTH = 50;
     private static final float PLUS_HEIGHT = 50;
-    private static final float PLUS_X1 = 750;
+    private static final float PLUS_X1 = 600;
     private static final float PLUS_Y1 = 380;
     private static final float PLUS_X2 = PLUS_X1 + PLUS_WIDTH;
     private static final float PLUS_Y2 = PLUS_Y1 + PLUS_HEIGHT;
+
+    //--clear
+    private static final float CLEAR_WIDTH = 102;
+    private static final float CLEAR_HEIGHT = 57;
+    private static final float CLEAR_X1 = 400;
+    private static final float CLEAR_Y1 = 323;
+    private static final float CLEAR_X2 = CLEAR_X1 + CLEAR_WIDTH;
+    private static final float CLEAR_Y2 = CLEAR_Y1 + CLEAR_HEIGHT;
+
+    //--clear table
+    private static final float CLEAR_TABLE_WIDTH = 200;
+    private static final float CLEAR_TABLE_HEIGHT = 67;
+    private static final float CLEAR_TABLE_X1 = 400;
+    private static final float CLEAR_TABLE_Y1 = 80;
+    private static final float CLEAR_TABLE_X2 = CLEAR_TABLE_X1 + CLEAR_TABLE_WIDTH;
+    private static final float CLEAR_TABLE_Y2 = CLEAR_TABLE_Y1 + CLEAR_TABLE_HEIGHT;
+
+    //--call the optimizer
+    private static final float START_WIDTH = 280;
+    private static final float START_HEIGHT = 70;
+    private static final float START_X1 = 120;
+    private static final float START_Y1 = 75;
+    private static final float START_X2 = START_X1 + START_WIDTH;
+    private static final float START_Y2 = START_Y1 + START_HEIGHT;
+
+    //--optimized purchase table
+    private static final float OPT_TABLE_X1 = 920;
+    private static final float OPT_TABLE_Y1 = 420;
 
     //--Text field
     private static final float TEXT_WIDTH = 200;
@@ -38,18 +68,22 @@ public class ShoppingListScreen extends ScreenAdapter {
     private static final float TEXT_X = PLUS_X1 - TEXT_WIDTH - 25;
     private static final float TEXT_Y = PLUS_Y1;
 
-
     private final Optimo game;
     private final ShoppingList shoppingList;
     private final GreenScore greenScore;
 
     private final Texture backArrowImg;
     private final Texture addElementImg;
+    private final Texture clearTextImg;
+    private final Texture clearTableImg;
+    private final Texture goShoppingImg;
 
     private final Skin uiSkin;
 
     private final List<String> list;
     private final TextField addElementTextField;
+
+    private final Table table;
 
     public ShoppingListScreen(Optimo game, ShoppingList shoppingList, GreenScore greenScore) {
         this.game = game;
@@ -58,12 +92,20 @@ public class ShoppingListScreen extends ScreenAdapter {
 
         backArrowImg = new Texture(Gdx.files.internal("BackArrow.png"));
         addElementImg = new Texture(Gdx.files.internal("AddElement.png"));
+        clearTextImg = new Texture(Gdx.files.internal("Clear.jpg"));
+        clearTableImg = new Texture(Gdx.files.internal("ClearTable.png"));
+        goShoppingImg = new Texture(Gdx.files.internal("StartButton.png"));
 
         uiSkin = new Skin(Gdx.files.internal("glassy-ui.json"));
 
         list = new List<>(uiSkin);
-
         addElementTextField = new TextField("", uiSkin);
+        table = new Table(uiSkin);
+        table.add(new Label("Brand", uiSkin)).width(135f).center();
+        table.add(new Label("Price", uiSkin)).width(135f).center();
+        table.add(new Label("CO2", uiSkin)).width(135f).center();
+        table.row();
+        table.setPosition(OPT_TABLE_X1, OPT_TABLE_Y1);
     }
 
     @Override
@@ -75,7 +117,42 @@ public class ShoppingListScreen extends ScreenAdapter {
                 if (x > BACK_ARROW_X1 && x < BACK_ARROW_X2 && renderY > BACK_ARROW_Y1 && renderY < BACK_ARROW_Y2) {
                     game.setScreen(new MainScreen(game, shoppingList, greenScore));
                 } else if (x > PLUS_X1 && x < PLUS_X2 && renderY > PLUS_Y1 && renderY < PLUS_Y2) {
-                    shoppingList.add(addElementTextField.getText());
+                    // add element to table
+                    if (addElementTextField.getText().length()> 0) {
+                        shoppingList.add(addElementTextField.getText());
+                        addElementTextField.setText("");
+                    }
+                } else if (x > CLEAR_X1 && x < CLEAR_X2 && renderY > CLEAR_Y1 && renderY < CLEAR_Y2) {
+                    // clear textbox
+                    addElementTextField.setText("");
+                } else if (x > CLEAR_TABLE_X1 && x < CLEAR_TABLE_X2 && renderY > CLEAR_TABLE_Y1 && renderY < CLEAR_TABLE_Y2) {
+                    // clear table
+                    shoppingList.clear();
+                } else if (x > START_X1 && x < START_X2 && renderY > START_Y1 && renderY < START_Y2) {
+                    // start the optimizer
+                    // Arrays.asList("Barley", "Apples", "Bananas")
+                    OptimoGenerator og = new OptimoGenerator(shoppingList, 500.0, 30000.0);
+                    try {
+                        String output = og.run();
+                        java.util.List<Map<String, String>> parsedOutput = og.parseOutput(output);
+                        table.reset();
+                        table.add(new Label("Brand", uiSkin)).width(135f).center();
+                        table.add(new Label("Price", uiSkin)).width(135f).center();
+                        table.add(new Label("CO2", uiSkin)).width(135f).center();
+                        table.row();
+                        int rows = 0;
+                        for (Map<String, String> row:parsedOutput) {
+                            if (rows > 10) break;
+                            table.add(new Label(row.get("brand"), uiSkin)).width(135f).center();
+                            table.add(new Label(row.get("p"), uiSkin)).width(135f).center();
+                            table.add(new Label(row.get("co2"), uiSkin)).width(135f).center();
+                            table.row();
+                            rows++;
+                        }
+                        table.setPosition(OPT_TABLE_X1, OPT_TABLE_Y1 - 22 * table.getRows()/2);
+                    } catch (IOException e) {
+                        System.out.println("Error: " + e.toString());
+                    }
                 }
                 return true;
             }
@@ -88,6 +165,11 @@ public class ShoppingListScreen extends ScreenAdapter {
                     addElementTextField.appendText(" ");
                 } else if (keycode == 67) {
                     addElementTextField.setText("");
+                } else if (keycode == 66) {
+                    if (addElementTextField.getText().length()> 0) {
+                        shoppingList.add(addElementTextField.getText());
+                        addElementTextField.setText("");
+                    }
                 }
                 return true;
             }
@@ -104,6 +186,10 @@ public class ShoppingListScreen extends ScreenAdapter {
         greenScore.render(game.getBatch());
         game.getBatch().draw(backArrowImg, BACK_ARROW_X1, BACK_ARROW_Y1, BACK_ARROW_WIDTH, BACK_ARROW_HEIGHT);
         game.getBatch().draw(addElementImg, PLUS_X1, PLUS_Y1, PLUS_WIDTH, PLUS_HEIGHT);
+        game.getBatch().draw(clearTextImg, CLEAR_X1, CLEAR_Y1, CLEAR_WIDTH, CLEAR_HEIGHT);
+        game.getBatch().draw(clearTableImg, CLEAR_TABLE_X1, CLEAR_TABLE_Y1, CLEAR_TABLE_WIDTH, CLEAR_TABLE_HEIGHT);
+        game.getBatch().draw(goShoppingImg, START_X1, START_Y1, START_WIDTH, START_HEIGHT);
+        table.draw(game.getBatch(), 1);
         renderShoppingList();
         renderTextField();
         game.getBatch().end();
@@ -115,7 +201,7 @@ public class ShoppingListScreen extends ScreenAdapter {
             listShopping[i] = shoppingList.get(i);
         }
         list.setItems(listShopping);
-        list.setPosition(300, 425 - 23 * list.getItems().size);
+        list.setPosition(150, 425 - 23 * list.getItems().size);
         list.setSize(200, 23 * list.getItems().size);
         list.draw(game.getBatch(), 1);
     }
